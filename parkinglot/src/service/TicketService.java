2558024d6fd54547a749f01e73f608859afb2556
Madhu696969 <2400030376@kluniversity.in>
2026-07-13 20:14:30 +0530

    project1 on LLD

--- parkinglot/src/service/TicketService.java
+++ parkinglot/src/service/TicketService.java
@@ -1,5 +1,33 @@
 package service;
+import java.time.LocalDateTime;
+
+import model.Slot;
+import model.Ticket;
+import model.Vehicle;
+import repository.TicketRepository;
 
 public class TicketService {
-
+	
+	private final TicketRepository ticketRepository;
+	private static int ticketCounter = 1;
+    public TicketService(TicketRepository ticketRepository) {
+        this.ticketRepository = ticketRepository;
+    }   
+	public Ticket generateTicket(Vehicle vehicle,Slot slot) {
+		Ticket ticket=new Ticket(ticketCounter++,vehicle,slot,LocalDateTime.now());
+		ticketRepository.addTicket(ticket);
+		return ticket;
+	}
+	public Ticket getTicket(int ticketId) {
+		return ticketRepository.getTicket(ticketId);
+	}
+	public void closeTicket(int ticketId) {
+		Ticket ticket = ticketRepository.getTicket(ticketId);
+	    if (ticket!=null){
+	        ticket.setExitTime(LocalDateTime.now());
+	    }
+	}
+	public void removeTicket(int ticketId) {
+		ticketRepository.removeTicket(ticketId);
+	}
 }
